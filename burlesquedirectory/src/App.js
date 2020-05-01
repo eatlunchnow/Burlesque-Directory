56db07b494da73d122cbfd25460e4e2065326277
eatlunchnow <eatlunchnow@gmail.com>
2020-04-30 17:29:53 -0700

    Updated App.js

--- burlesquedirectory/src/App.js
+++ burlesquedirectory/src/App.js
@@ -1,11 +1,45 @@
-import React from "react"
+import React from 'react';
+import './styles.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './contents/Home';
+import About from './contents/About';
+import Form from './contents/Form';
+import Search from './contents/Search';
+import Contact from './contents/Contact';
+import Form2 from './contents/Form2';
+
+
 
 function App() {
-    return (
-        <div>
-            <h1>Burlesque Directory</h1>
-        </div>
-    )
-}
-
-export default App
+  return (
+    <Router>
+    <div className="App">
+    <Navbar />
+    <Route exact path="/">
+    <Home />
+    </Route>
+    <Route path="/about">
+    <About />
+    </Route>
+    <Route path="/form">
+    <Form />
+    </Route>
+    <Route path="/search">
+    <Search />
+    </Route>
+    <Route path="/contact">
+    <Contact />
+    </Route>
+    <Route path="/form2">
+    <Form2 />
+    </Route>
+    </div>
+    </Router>
+    );
+  }
+  
+  export default App;
