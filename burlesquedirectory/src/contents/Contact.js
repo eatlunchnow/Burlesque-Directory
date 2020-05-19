62aaff439499fb2aae88b45efd23c9f922eac0ec
eatlunchnow <eatlunchnow@gmail.com>
2020-05-19 16:59:30 -0700

    Added ContactForm component to Contact page

--- burlesquedirectory/src/contents/Contact.js
+++ burlesquedirectory/src/contents/Contact.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import ContactForm from "../components/ContactForm";
 
 class Contact extends Component {
     render() {
         return (
             <div className="condiv">
             <h1>Contact</h1>
-            <h3>Email:  burlesquedirectorycontact@gmail.com</h3>
+            <h3>If you would like to contact us please email us at burlesquedirectorycontact@gmail.com or fill
+              out the form. 
+            </h3>
+            <ContactForm />
+
             </div>
             )
         }
