ec12e412184b8957c9268752af5aa71982dabee6
eatlunchnow <eatlunchnow@gmail.com>
2020-05-21 11:42:26 -0700

    Added Logo component

--- burlesquedirectory/src/components/Navbar.js
+++ burlesquedirectory/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navitem from './Navitem';
 import Logo from '../components/Logo'
-
+//import Auth from '../components/Auth'
 
 class Navbar extends Component {
     constructor(props)
